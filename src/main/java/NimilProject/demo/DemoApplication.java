6bc45675cd09e34521f1e31573bddee496936723
rajendrakumar p <rajendrakumarp99@gmail.com>
2025-01-16 12:54:36 +0530

    Update DemoApplication.java
    Vaiable changes

--- src/main/java/NimilProject/demo/DemoApplication.java
+++ src/main/java/NimilProject/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	int i=20;
-	int j=40;
+	int j=30;
 	String s="Rajnedra";
 
 	public static void main(String[] args) {
